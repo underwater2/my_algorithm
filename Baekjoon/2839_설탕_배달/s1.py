25ac4c16db75f55705fd0132823860f806324ab5
underwater <shaian2@naver.com>
2022-04-18 13:50:36 +0900

    백준 2839 풀이 설명, 다른 풀이 추가

--- Baekjoon/2839_설탕_배달/s1.py
+++ Baekjoon/2839_설탕_배달/s1.py
@@ -1,4 +1,6 @@
 # Greedy
+# 5크기 봉지를 최대로 설정한 후에, 한개씩 줄여가며 3크기 봉지에 딱 들어 맞는지 조사한다.
+# 맞는 조합이 있으면 그 시점에 검색을 중단하고 답을 출력한다.
 
 import sys
 sys.stdin = open('input.txt')
@@ -15,3 +17,20 @@ for a in range(A, -1, -1):
     else:
         continue
 print(result)
+
+
+# # 검색해서 나온 다른 풀이. 이 풀이도 신기했다
+# num = int(input())
+# count = 0
+#
+# while num >= 0:
+#   if num % 5 == 0:
+#     count += int(num // 5)
+#     print(count)
+#     break
+#
+#   num -= 3
+#   count += 1
+#
+# else:
+#   print(-1)
