566e424903e08f8425ea6d613a6356a4b96383e0
underwater <shaian2@naver.com>
2022-07-30 01:11:56 +0900

    프로그래머스: 메뉴 리뉴얼 정답

--- Programmers/메뉴 리뉴얼/s1.py
+++ Programmers/메뉴 리뉴얼/s1.py
@@ -1,30 +1,38 @@
+# 문제를 이해하기 어려웠다
+    # 각 메뉴구성 개수 마다 빈도가 가장 많은 조합만 return
+
 from itertools import combinations
 
 def solution(orders, course):
     answer = []
-    # menus = dict()
-    menulist = list()
+    menus = dict()
     course.sort(reverse=True)
     for cnt in course:
         for order in orders:
             for comb in combinations(sorted(order), cnt):
-                for prem in menulist:
-                    if comb in prem:
-                        break
+                menu = ''.join(list(comb))
+                if menu in menus:
+                    menus[menu] += 1
                 else:
-                    menulist.append(comb)
+                    menus[menu] = 1
 
-    for prem in menulist:
-        if menulist.count(prem) >= 2:
-            answer.append(''.join(prem))
+    sorted_menus = sorted(menus.items(), key = lambda item: item[1], reverse = True)
+    print(sorted_menus)
+    maxN = [0] * 11
+    for menu in sorted_menus:
+        if not course or menu[1] < 2:
+            break
+        elif len(menu[0]) in course:
+            maxN[len(menu[0])] = menu[1]
+            course.remove(len(menu[0]))
+        else:
+            continue
+    print(maxN)
 
-    # menu = ''.join(list(comb))
-    # if menu in menus:
-    #     menus[menu] += 1
-    # else:
-    #     menus[menu] = 1
+    for n in range(2, 11):
+        if maxN[n]:
+            for menu in sorted_menus:
+                if len(menu[0]) == n and menu[1] == maxN[n]:
+                    answer.append(menu[0])
 
-    # for key in menus:
-    #     if menus[key] >= 2:
-    #         answer.append(key)
-    return sorted(answer)
+    return sorted(set(answer))
