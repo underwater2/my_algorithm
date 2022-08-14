9a689b8fb00b9c87684032830f2708cae23bb9de
underwater <shaian2@naver.com>
2022-08-14 23:19:05 +0900

    프로그래머스: [튜플] 다른사람풀이 정리

---
+++ Programmers/Level 2/튜플/다른사람풀이1.py
@@ -0,0 +1,12 @@
+# 출처: 프로그래머스 다른 사람의 풀이
+# Counter() 사용하여 코드가 더 짧아졌다.
+
+def solution(s):
+
+    s = Counter(re.findall('\d+', s))  # Counter(): iterable 객체를 넣으면 각 요소들의 빈도를 딕셔너리로 반환해준다
+    return list(map(int, [k for k, v in sorted(s.items(), key=lambda x: x[1], reverse=True)]))
+    # s.items(): 리스트 안에 딕셔너리의 key-value 쌍이 튜플 형태로 들어있다.
+    # for문으로 순회하는 대신 map을 사용함
+
+import re
+from collections import Counter
