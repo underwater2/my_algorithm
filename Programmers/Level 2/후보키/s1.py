3c0799f7c7b5eadce6f7084f6d21529d0af7f027
underwater <shaian2@naver.com>
2022-08-18 18:22:16 +0900

    프로그래머스: [후보키]

--- Programmers/Level 2/후보키/s1.py
+++ Programmers/Level 2/후보키/s1.py
@@ -1,54 +1,34 @@
-# 미통과. 75%만 맞음ㅜㅜ
+# 유일성에서 중복 검사, 최소성에서 이미 있는 조합에 포함되는지 검사하는 게 까다로웠다.
+# tuple('abcd') => ('a', 'b', 'c', 'd')
+# {2, 3} < {2, 3, 4} => True
 
-# 참고 코드
-# https://codlingual.tistory.com/161
-
-# # set.isSubset()
-# x = {"홈짱닷컴", "Homzzang.com", "코딩강의"}
-# y = {"홈짱닷컴", "Homzzang.com", "코딩강의", "2012"}
-# res = x.issubset(y)  # x가 y의 서브 set 맞냐?
-# print(res)
-#
-# 결과값: True
 
 from itertools import combinations
 
 def solution(relation):
-    # 속성 갯수를 가지고 조합을 구해야한다.
-    N = len(relation[0])  # 속성 갯수
-    M = len(relation)  # 튜플 갯수
-    # 속성의 인덱스는 1부터 N까지
-    keys = [i for i in range(0, N)]  # N=4면 [0, 1, 2, 3]
-    candidate_keys = []
-    # 속성들의 조합
-    for i in range(1, N+1):  # 조합 요소 수가 1인 것부터 4인 것까지 순회
-        for j in combinations(keys, i):  # i=2면 [(0,1), (0,2), (0,3), ...]
-                                        # j = (0,1)
-            # 유일성을 가지는 조합인지 검사
-            tuple_list = list()
-            for k in range(M):
-                temp = [relation[k][l] for l in j]
-                if temp in tuple_list:
+    answer = 0
+    candidates = []  # 후보키 속성 조합을 set 요소로 저장
+    for i in range(1, len(relation[0]) + 1):  # 전체 속성 중 몇 개(i)를 뽑아 조합을 만들 건지
+        for comb in combinations(list(range(len(relation[0]))), i):  # 속성 인덱스들을 가지고 조합 만들기
+            flag2 = 1
+            for cand in candidates:  # (최소성 검사) 이미 있는 key 조합에 포함되는지 검사
+                if set(comb) > cand:
+                    flag2 = 0
                     break
-                else:
-                    tuple_list.append(temp)
-            # 최소성을 가지는 조합인지 검사
-            if len(tuple_list) == M:
-                for keys in candidate_keys:
-                    if set(keys).issubset(j):  # keys가 요소 수가 적은 조합이기 때문에 순서가 이럼
+            if flag2:  # 최소성 만족하면
+                overlap = list()
+                flag = 1
+                for row in relation:  # 행 순회
+                    data = list()
+                    for c in comb:
+                        data.append(row[c])
+                    if data in overlap:  # (유일성 검사) 다른 행의 속성 조합과 같은지 검사
+                        flag = 0
                         break
-                else:
-                    candidate_keys.append(j)
-    return len(candidate_keys)
+                    else:
+                        overlap.append(data)
+                if flag:
+                    candidates.append(set(comb))
+                    answer += 1
 
-
-
-relation = [
-    ["100","ryan","music","2"],
-    ["200","apeach","math","2"],
-    ["300","tube","computer","3"],
-    ["400","con","computer","4"],
-    ["500","muzi","music","3"],
-    ["600","apeach","music","2"]
-]
-print(solution(relation))
+    return answer
