1119c67d423159bfe9e77fafe5658ad4902e8bd4
underwater <shaian2@naver.com>
2022-09-27 22:56:48 +0900

    프로그래머스: [다음 큰 숫자] 파이썬 풀이

--- Programmers/Level 2/다음 큰 숫자/s1.py
+++ Programmers/Level 2/다음 큰 숫자/s1.py
@@ -1,16 +1,23 @@
 def func(n, cnt):
     n += 1
-    bn = bin(answer)[2:]
+    bn = bin(n)[2:]
     cnt1 = bn.count('1')
     if cnt1 == cnt:
-        return int(bn, 2)
+        return n
     elif cnt1 > cnt:
         return func(n, cnt)
     else:
-        continue
+        num = cnt - cnt1
+        newn = ''
+        for i in range(len(bn) - 1, -1, -1):
+            if num > 0 and bn[i] == '0':
+                newn = '1' + newn
+                num -= 1
+            else:
+                newn = bn[i] + newn
+        return int('0b' + newn, 2)
 
 
 def solution(n):
-    answer = 4
-    print(bin(answer))
+    answer = func(n, bin(n)[2:].count('1'))
     return answer
